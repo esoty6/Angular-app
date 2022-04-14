775c7f42f3845d6c144ee09ffb5444dafdadcf6f
Kamil Witek <kw113970@stud.ur.edu.pl>
2022-04-15 00:34:57 +0200

    Initialized with ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {CollapseModule} from "ngx-bootstrap/collapse";
+import {NgxNavbarModule} from "ngx-bootstrap-navbar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    CollapseModule,
+    NgxNavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
